bb050eecd622f842537fec42c57c0cb993e278a5
nabilmuafa <nmuafa@gmail.com>
2023-09-19 21:39:42 +0700

    develop up to form

--- main/views.py
+++ main/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core import serializers
+from main.forms import ItemForm
+from django.urls import reverse
+from main.models import Item
 
 def show_main(request):
+    items = Item.objects.all()
+
     context = {
         'app_name': "Warehouse Inventory",
         'name': "Muhammad Nabil Mu'afa",
-        'class': "PBP C"
+        'class': "PBP C",
+        'items': items
     }
 
     return render(request, "main.html", context)
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_item.html", context)
+
