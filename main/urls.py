6471b0a7f0c1ff35cbf01ca0f415fa2196bffb1b
nabilmuafa <nmuafa@gmail.com>
2023-11-22 07:17:35 +0700

    add urls for flutter forms

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from main.views import show_main, create_item, show_xml, show_json, \
     show_json_by_id, show_xml_by_id, register, login_user, logout_user, \
-    decrement, increment, delete, get_json_by_user, add_items_ajax
+    decrement, increment, delete, get_json_by_user, add_items_ajax, create_product_flutter
 
 app_name = 'main'
 
@@ -11,6 +11,8 @@ urlpatterns = [
     path('login/', login_user, name="login"),
     path('logout/', logout_user, name="logout"),
     path('create-item/', create_item, name="create_item"),
+    path('create-flutter/', create_product_flutter,
+         name="create_product_flutter"),
     path('delete/', delete, name="delete"),
     path('dec/', decrement, name="dec"),
     path('inc/', increment, name="inc"),
